92a73e6d20e39750b80be4ba9fa939c0cdd76d17
nilofarshaikh12 <nilofar.shaikh@bnt-soft.com>
2025-05-13 12:32:02 +0530

    adding about,contact.header,home,login and performed if,for,switch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'app',component:AppComponent},
+  {path:'about',component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'login',component:LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  //imports:[NgIf]
+
 })
 export class AppComponent {
   title = 'angularDemo2';
+  show=true
+
+  //array
+  students=["ram","sam","neha","priya"];
+
+  //array of objects
+  studentdata=[
+    {
+      name:"ram",
+      age:20
+    },
+    {
+      name:"sam",
+      age:30
+    }
+  ]
+
+//ngIf
+  block=0
+  updateBlock(){
+    this.block++;
+  }
+
+  //switch case
+
+  color="black"
+
+  changeColor(color:string){
+    this.color=color
+  }
 }
